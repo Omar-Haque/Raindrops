18512c58fe6d075d30cb172ea625e9bf71675c22
WitchesMe <54588062+WitchesMe@users.noreply.github.com>
2020-02-04 12:21:02 +0530

    Update raindrops.js

--- raindrops.js
+++ raindrops.js
@@ -1,21 +1,8 @@
-class Raindrop{
-  constructor(x,y,width,height){
-      this.body = rect(x,y,width,height);
-      this.height = height;
-      this.width = width;
-      this.x = x;
-      this.y = y;
-  }
-
-  fall(speed){
-      this.y +=speed;
-  }
-
-  display(){
-      push();
-      fill("yellow");
-      rectMode(CENTER);
-      rect(this.x, this.y, this.width,this.height);
-      pop();
-  }
+class Raindrop extends baseClass{
+    constructor(x,y,width,height){
+        super(x,y,width,height);
+    }
+    fall(speed){
+        this.y +=speed;
+    }
 }
